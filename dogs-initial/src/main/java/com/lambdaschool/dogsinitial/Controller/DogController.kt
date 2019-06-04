19c05ccf4928874d33ed4c4542b9b8d0a93a5406
Jake Esveld <thejakeesveld@gmail.com>
2019-06-04 14:38:41 -0700

    Try setting up template

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/Controller/DogController.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/Controller/DogController.kt
@@ -10,24 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 
 @RestController
 @RequestMapping("/dogs")
 class DogController {
-    // localhost:8080/dogs/dogs
+    // localhost:8080/dogs.html/dogs.html
     val allDogs: ResponseEntity<*>
         @GetMapping(value = ["/dogs"])
         get() = ResponseEntity(DogsinitialApplication.getOurDogList().dogList, HttpStatus.OK)
 
 
-    // localhost:8080/dogs/{id}
+    // localhost:8080/dogs.html/{id}
     @GetMapping(value = ["/{id}"])
     fun getDogDetail(@PathVariable id: Long): ResponseEntity<*> {
         val rtnDog = DogsinitialApplication.getOurDogList().findDog(CheckDog { d -> d.id == id }) ?: throw ResourceNotFoundException(message = "Dog with id $id cannot be found", cause = null)
         return ResponseEntity<Dog>(rtnDog, HttpStatus.OK)
     }
 
-    // localhost:8080/dogs/breeds/{breed}
+    // localhost:8080/dogs.html/breeds/{breed}
     @GetMapping(value = ["/breeds/{breed}"])
     fun getDogBreeds(@PathVariable breed: String): ResponseEntity<*> {
 //        val rtnDogs = DogsinitialApplication.getOurDogList().findDogs({ d -> d.getBreed().toUpperCase().equals(breed.toUpperCase()) })
@@ -36,6 +37,14 @@ class DogController {
             throw ResourceNotFoundException(message = "Could not find $breed", cause = null)
         }
         return ResponseEntity(rtnDogs, HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/breedtable"])
+    fun getDogBreedTable():ModelAndView{
+        val mav: ModelAndView = ModelAndView("dogs")
+        mav.addObject("dogList", DogsinitialApplication.getOurDogList().dogList)
+
+        return mav
     }
 
 
--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.kt
@@ -3,7 +3,10 @@ package com.lambdaschool.dogsinitial
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.servlet.DispatcherServlet
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
+@EnableWebMvc
 @SpringBootApplication
 open class DogsinitialApplication {
 
@@ -15,6 +18,10 @@ open class DogsinitialApplication {
         fun main(args: Array<String>) {
             ourDogList = DogList()
             SpringApplication.run(DogsinitialApplication::class.java, *args)
+            val ctx = SpringApplication.run(DogsinitialApplication::class.java, *args)
+
+            val dispatcherServlet = ctx.getBean("dispatcherServlet") as DispatcherServlet
+            dispatcherServlet.setThrowExceptionIfNoHandlerFound(true)
         }
 
         fun getOurDogList() : DogList = ourDogList
