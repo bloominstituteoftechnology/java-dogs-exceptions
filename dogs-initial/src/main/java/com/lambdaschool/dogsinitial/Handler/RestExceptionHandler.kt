e6dcea212ddba093e3b2365db7c1d387fa4d56a3
Jake Esveld <thejakeesveld@gmail.com>
2019-06-04 13:34:43 -0700

    Add the rest of the Exception handlers

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/Handler/RestExceptionHandler.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/Handler/RestExceptionHandler.kt
@@ -2,12 +2,16 @@ package com.lambdaschool.dogsinitial.Handler
 
 import com.lambdaschool.dogsinitial.Exception.ResourceNotFoundException
 import com.lambdaschool.dogsinitial.Model.ErrorDetail
+import org.springframework.beans.TypeMismatchException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.NoHandlerFoundException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.sql.Timestamp
 import java.util.*
@@ -33,6 +37,23 @@ class RestExceptionHandler: ResponseEntityExceptionHandler(){
         return ResponseEntity<Any>(errorDetail, null, HttpStatus.NOT_FOUND)
     }
 
+    override fun handleTypeMismatch(ex: TypeMismatchException, headers: HttpHeaders, status: HttpStatus, request: WebRequest): ResponseEntity<Any> {
+        val errorDetail = ErrorDetail(ex.propertyName.toString(),
+                HttpStatus.BAD_REQUEST.value(),
+                ex.message.toString(),
+                Timestamp(System.currentTimeMillis()),
+                request.getDescription(true))
 
+        return ResponseEntity(errorDetail, null, HttpStatus.BAD_REQUEST)
+    }
 
+    override fun handleNoHandlerFoundException(ex: NoHandlerFoundException, headers: HttpHeaders, status: HttpStatus, request: WebRequest): ResponseEntity<Any> {
+        val errorDetail = ErrorDetail(ex.requestURL,
+                HttpStatus.NOT_FOUND.value(),
+                request.getDescription(true),
+                Timestamp(System.currentTimeMillis()),
+                "Handler not found. Check valid URI")
+
+        return ResponseEntity(errorDetail, null, HttpStatus.NOT_FOUND)
+    }
 }
